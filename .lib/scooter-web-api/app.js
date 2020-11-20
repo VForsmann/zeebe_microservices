c86a7166726d4a543c00c4ce773b30d7fc2efb1f
Vitus Forsmann <vitus.forsmann@hotmail.de>
2020-11-20 23:49:05 +0100

    microservice startup check

--- .lib/scooter-web-api/app.js
+++ .lib/scooter-web-api/app.js
@@ -2,14 +2,8 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const bodyParser = require("body-parser");
-
 const events = require("events");
-const ZB = require("zeebe-node");
-const zbc = new ZB.ZBClient("zeebe:26500");
-
-const bookScooter = zbc.createWorker("book_scooter", book_scooter);
-const sendSuccessNotification = zbc.createWorker("send_success_notification", send_success_notification);
-const sendRejectionNotification = zbc.createWorker("send_rejection_notification", send_rejection_notification);
+const http2 = require("http2");
 const eventEmitter = new events.EventEmitter();
 const port = 8090;
 
@@ -27,31 +21,30 @@ app.post("/book", async (req, res) => {
 });
 
 app.ws("/", function (ws, req) {
-	const successNotification = () => {
-		console.log("websockets sends success");
-		ws.send("SUCCESS");
-	}
+    const successNotification = () => {
+        console.log("websockets sends success");
+        ws.send("SUCCESS");
+    };
     eventEmitter.addListener("send_success_notification", () => {
-		if(ws.readyState === ws.OPEN) {
-			successNotification()
-		} else {
-			eventEmitter.removeListener("send_success_notification", successNotification);
-		}
-	});
-	
-	const rejectionNotification = () => {
-		console.log("websockets sends rejection");
-		ws.send("REJECTED");
-	}
+        if (ws.readyState === ws.OPEN) {
+            successNotification();
+        } else {
+            eventEmitter.removeListener("send_success_notification", successNotification);
+        }
+    });
+
+    const rejectionNotification = () => {
+        console.log("websockets sends rejection");
+        ws.send("REJECTED");
+    };
 
     eventEmitter.addListener("send_rejection_notification", () => {
-		if(ws.readyState === ws.OPEN) {
-			rejectionNotification()
-		} else {
-			eventEmitter.removeListener("send_rejection_notification", rejectionNotification);
-		}
-
-	})
+        if (ws.readyState === ws.OPEN) {
+            rejectionNotification();
+        } else {
+            eventEmitter.removeListener("send_rejection_notification", rejectionNotification);
+        }
+    });
 });
 
 app.listen(port, () => {
@@ -74,3 +67,36 @@ function send_rejection_notification(job, complete, worker) {
     eventEmitter.emit("send_rejection_notification");
     complete.success();
 }
+
+// Some docker-stuff: Full Start of Server when mongodb is available
+async function alive() {
+    await new Promise((resolve, reject) => {
+        const func = () => {
+
+			const client = http2.connect("http://zeebe:26500");
+				
+			client.on("error", () => {
+				console.log("WAITING FOR ZEEBE TO START");
+				setTimeout(() => func(), 1000);
+			});
+
+			client.on("socketError", () => {
+				console.log("WAITING FOR ZEEBE TO START");
+				setTimeout(() => func(), 1000);
+			})
+        };
+        func();
+    });
+}
+
+const startUpZeebeClient = async () => {
+    await alive();
+    const ZB = require("zeebe-node");
+    const zbc = new ZB.ZBClient("zeebe:26500");
+
+    const bookScooter = zbc.createWorker("book_scooter", book_scooter);
+    const sendSuccessNotification = zbc.createWorker("send_success_notification", send_success_notification);
+    const sendRejectionNotification = zbc.createWorker("send_rejection_notification", send_rejection_notification);
+};
+
+startUpZeebeClient();
